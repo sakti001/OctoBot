fb5c607a75d7806522e5cfc29cb949762f7af350
Octo <nezd@protonmail.com>
2017-04-02 12:32:19 +0300

    Reddit module created

---
+++ plugins/Reddit.py
@@ -0,0 +1,43 @@
+"""
+Reddit module
+"""
+import logging
+
+from telegram import Bot, Update
+from praw import Reddit
+from settings import REDDITID, REDDITUA, REDDITSECRET # pylint: disable=E0611
+
+import constants # pylint: disable=E0401
+LOGGER = logging.getLogger("Reddit")
+REDDIT = Reddit(client_id=REDDITID,
+                client_secret=REDDITSECRET,
+                user_agent=REDDITUA
+               )
+
+def preload(*_):
+    """
+    This loads whenever plugin starts
+    Even if you dont need it, you SHOULD put at least
+    return None, otherwise your plugin wont load
+    """
+    return
+
+def posts(bot: Bot, update: Update, user, args): # pylint: disable=W0613
+    """
+    /r/subreddit
+    """
+    sub = update.message.text.split("/")[2]
+    subreddit = REDDIT.subreddit(sub)
+    message = "Hot posts in <b>/r/%s</b>:\n\n" % sub
+    for post in subreddit.hot(limit=10):
+        message += ' • <a href="%s">%s</a>\n' % (post.shortlink, post.title)
+    return message, constants.HTMLTXT
+
+COMMANDS = [
+    {
+        "command":"/r/",
+        "function":posts,
+        "description":"10 hot posts in specifed subreddit",
+        "inline_support":True
+    }
+]
--- settings.example.py
+++ settings.example.py
@@ -5,3 +5,8 @@ TOKEN = "PUTYOURTOKENHERE"
 # Octeon needs this channel to make inline images work
 # Put channel ID here
 CHANNEL = 0
+# This is required for "Reddit" plugin
+# 'script' level required
+REDDITID = ""
+REDDITSECRET = ""
+REDDITUA = ""
