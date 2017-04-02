bfbdbed836cfc27a5be214a0f15184f036f68336
Octo <nezd@protonmail.com>
2017-04-02 08:49:48 +0300

    Word swap ported

---
+++ plugins/Word_Swap.py
@@ -0,0 +1,49 @@
+"""
+Word swap module
+"""
+import logging
+
+from telegram import Bot, Update
+from telegram.ext import Filters, MessageHandler, Updater
+
+import constants # pylint: disable=E0401
+
+LOGGER = logging.getLogger("Word Swap Module")
+
+
+def preload(updater: Updater, level):
+    """
+    This loads whenever plugin starts
+    Even if you dont need it, you SHOULD put at least
+    return None, otherwise your plugin wont load
+    """
+    return
+def wordsw(bot: Bot, update: Update, user, args):
+    """Word swap command"""
+    msg = update.message
+    txt = msg.text
+    logging.debug(msg)
+    if msg.reply_to_message is not None:
+        if txt.startswith("/s"):
+            if not msg.reply_to_message.from_user.name == bot.getMe().name:
+                txt = txt.split('/')
+                origword = txt[2]
+                swap = txt[3]
+                if len(origword) > 0 and len(origword) < 15:
+                    if len(swap) > 0 and len(swap) < 15:
+                        msg.reply_to_message.reply_text(
+                            "Hello, {}\nDid you mean:\n{}".format(
+                                msg.reply_to_message.from_user.first_name,
+                                msg.reply_to_message.text.replace(origword, swap)
+                            )
+                        )
+    return None, constants.NOTHING
+
+COMMANDS = [
+    {
+        "command":"/s/",
+        "function":wordsw,
+        "description":"Swaps word!",
+        "inline_support": False
+    }
+]
