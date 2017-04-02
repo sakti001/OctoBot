b59ae050d3ede268e91ecf684dd66b772d7ce189
Octo <nezd@protonmail.com>
2017-04-02 08:48:08 +0300

    TImezone ported

---
+++ plugins/timezone.py
@@ -0,0 +1,28 @@
+"""Timezones"""
+from telegram import Bot, Update
+import pendulum
+import constants
+from pytzdata.exceptions import TimezoneNotFound
+TIMEFORMAT = '%A %d%tof %B %Y %H:%M:%S'
+def preload(*_):
+    """We dont need preload, so it is disabled"""
+    return
+
+def timezonecmd(_: Bot, update: Update, user, args):
+    """/tz command"""
+    timezone = " ".join(args)
+    try:
+        timezone = pendulum.now(timezone)
+    except TimezoneNotFound:
+        return "⚠You specifed unknown timezone", constants.TEXT
+    else:
+        return timezone.timezone_name + ": " + timezone.format(TIMEFORMAT), constants.TEXT
+
+COMMANDS = [
+    {
+        "command":"/tz",
+        "function":timezonecmd,
+        "description":"Sends info about specifed zone(for example:Europe/Moscow)",
+        "inline_support": True
+    }
+]
