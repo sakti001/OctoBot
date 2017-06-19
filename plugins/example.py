0a3120ac1f5c31de434a189b4f46c39c51b90643
Yan <nezd@protonmail.com>
2017-06-19 17:40:51 +0300

    Fixed example.py

--- plugins/example.py
+++ plugins/example.py
@@ -3,7 +3,7 @@ PLUGINVERSION = 2
 # Always name this variable as `plugin`
 # If you dont, module loader will fail to load the plugin!
 plugin = octeon.Plugin()
-@plugin.command(name="/hi",
+@plugin.command(command="/hi",
                 description="Says 'Hi, %username%'",
                 inline_supported=True)
 def hi(self, args, update, user):
