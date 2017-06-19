07dd974f0aaab26c57604e5d4acb08783b9e68a0
Yan <nezd@protonmail.com>
2017-06-19 17:40:43 +0300

    Plugin class

--- octeon.py
+++ octeon.py
@@ -1,6 +1,7 @@
 """
 Octeon stuff
 """
+
 class message:
     """
     Base message class
@@ -8,10 +9,31 @@ class message:
     Raises:
         TypeError on incompatible conditions, like photo and parse_mode
     """
-    def __init__(text="", photo=None, inline_keyboard=None, parse_mode=None):
+    def __init__(self,
+                 text="", 
+                 photo=None, 
+                 inline_keyboard=None, 
+                 parse_mode=None,
+                 failed=False):
         self.text = text
+        self.failed = fail
         self.photo = photo
         self.inline_keyboard = inline_keyboard
         self.parse_mode = parse_mode
         if photo and parse_mode:
             raise TypeError("parse_mode and photo cant be used at same time!")
+
+class Plugin:
+    """Octeon plugin base"""
+    def __init__(self):
+        self.commands = []
+    
+    def command(self, command, description, inline_supported=True):
+        def tags_decorator(func):
+            self.commands.append({
+                "command":command,
+                "description":description,
+                "function":func,
+                "inline_support":inline_supported
+            })
+        return tags_decorator
