71ab32e12a8189496896471710f9f66431913fdc
Yan <nezd@protonmail.com>
2017-07-06 18:54:30 +0300

    Re-write /slap, move it from IRC plugin, add new messages thanks to Shadowhand

--- plugins/IRC.py
+++ plugins/IRC.py
@@ -3,12 +3,7 @@ from random import choice
 
 from telegram import Bot, Update
 import constants
-SLAPMSGS = ["{} backstabs {}",
-            "{} brutally rapes {}",
-            "{} gassed {} like Hitler gassed jews",
-            "{} pwned {}",
-            "{} noscoped {}",
-            "{} hacked {}"]
+
 
 def preload(*_):
     """We dont need anything"""
@@ -23,23 +18,6 @@ def irc_me(bot: Bot, update: Update, user, args):
     return None, constants.NOTHING
 
 
-def slap(bot: Bot, update: Update, user, args):
-    """/slap"""
-    msg = choice(SLAPMSGS)
-    text = " ".join(update.message.text.split(" ")[1:])
-    user2 = update.message.from_user.name
-    if update.message.reply_to_message is None:
-        user2 = bot.getMe().name
-        user1 = update.message.from_user.name
-    else:
-        user1 = update.message.reply_to_message.from_user.name
-    if text != "":
-        user1 = text
-        user2 = update.message.from_user.name
-    bot.sendMessage(chat_id=update.message.chat_id,
-                    text=msg.format(user2, user1))
-    return None, constants.NOTHING
-
 COMMANDS = [
     {
         "command":"/me",
@@ -47,10 +25,4 @@ COMMANDS = [
         "description":"IRC /me-alike command",
         "inline_support": False
     },
-    {
-        "command":"/slap",
-        "function":slap,
-        "description":"IRC /slap command",
-        "inline_support": False
-    }
 ]
