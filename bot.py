2b7d5f7f9829f004889d30dedbcc8e06d995533c
Yan <nezd@protonmail.com>
2017-07-16 16:22:39 +0300

    Transform old message format into new one using class method

--- bot.py
+++ bot.py
@@ -36,6 +36,8 @@ COMMANDS = moduleloader.gen_commands(PLUGINS)
 INLINE = moduleloader.gen_inline(PLUGINS)
 REGEXHAND = moduleloader.gen_messages(PLUGINS)
 CMDDOCS = moduleloader.generate_docs(PLUGINS)
+
+
 def tracker(_: Bot, update: Update, __, ___):
     reply = update.message.reply_to_message
     if reply:
@@ -43,6 +45,7 @@ def tracker(_: Bot, update: Update, __, ___):
             return "<pre>" + escape(pformat(TRACKER[reply.message_id])) + "</pre>", constants.HTMLTXT
         else:
             return "I dont remember sending this message..."
+
 
 def command_handle(bot: Bot, update: Update):
     """
@@ -56,8 +59,10 @@ def command_handle(bot: Bot, update: Update):
     commanddata = update.message.text.split()[0].split('@')
     if (len(commanddata) >= 2 and commanddata[1] == bot.username) or (len(commanddata) == 1):
         for command in COMMANDS:
-            state_only_command = update.message.text == command or update.message.text.startswith(command + " ")
-            state_word_swap = len(update.message.text.split("/")) > 2 and update.message.text.startswith(command)
+            state_only_command = update.message.text == command or update.message.text.startswith(
+                command + " ")
+            state_word_swap = len(update.message.text.split(
+                "/")) > 2 and update.message.text.startswith(command)
             state_mention_command = update.message.text.startswith(command + "@")
             if state_only_command or state_word_swap or state_mention_command:
                 if len(TRACKER) > 100:
@@ -75,72 +80,35 @@ def command_handle(bot: Bot, update: Update):
                     return
                 elif not isinstance(reply, octeon.message):
                     # Backwards compability
-                    LOGGER.warning("Old message reply format! Please update it to new one")
-                    if isinstance(reply, str):
-                        msg = message.reply_text(reply)
-                    elif reply is None:
-                        return
-                    elif reply[1] == constants.TEXT:
-                        msg = message.reply_text(
-                            reply[0]
-                        )
-                    elif reply[1] == constants.MDTEXT:
-                        msg = message.reply_text(
-                            reply[0],
-                            parse_mode="MARKDOWN"
-                        )
-                    elif reply[1] == constants.HTMLTXT:
-                        msg = message.reply_text(
-                            reply[0],
-                            parse_mode="HTML"
-                        )
-                    elif reply[1] == constants.NOTHING:
-                        pass
-                    elif reply[1] == constants.PHOTO:
-                        msg = message.reply_photo(
-                            reply[0]
-                        )
-                    elif reply[1] == constants.PHOTOWITHINLINEBTN:
-                        msg = message.reply_photo(reply[0][0],
-                                                    caption=reply[0][1],
-                                                    reply_markup=reply[0][2])
-                    else:
-                        raise NotImplementedError("%s type is not implemented" % reply[1])
-                    if "failed" in reply:
-                        msdict = msg.to_dict()
-                        msdict["chat_id"] = msg.chat_id
-                        msdict["user_id"] = update.message.from_user.id
-                        kbrmrkup = InlineKeyboardMarkup([[InlineKeyboardButton("Delete this message", 
-                                                        callback_data="del:%(chat_id)s:%(message_id)s:%(user_id)s" % msdict)]])
-                        msg.edit_reply_markup(reply_markup=kbrmrkup)
-                else:
-                    if reply.photo:
-                        msg = message.reply_photo(reply.photo,
-                                                  caption=reply.text,
-                                                  reply_markup=reply.inline_keyboard)
-                    elif reply.file:
-                        msg = message.reply_document(document=reply.file,
-                                                     caption=reply.text,
-                                                     reply_markup=reply.inline_keyboard)
-                    else:
-                        msg = message.reply_text(reply.text,
-                                                 parse_mode=reply.parse_mode,
+                    reply = octeon.message.from_old_format(reply)
+                if reply.photo:
+                    msg = message.reply_photo(reply.photo,
+                                              caption=reply.text,
+                                              reply_markup=reply.inline_keyboard)
+                elif reply.file:
+                    msg = message.reply_document(document=reply.file,
+                                                 caption=reply.text,
                                                  reply_markup=reply.inline_keyboard)
-                    if reply.failed:
-                        msdict = msg.to_dict()
-                        msdict["chat_id"] = msg.chat_id
-                        msdict["user_id"] = update.message.from_user.id
-                        kbrmrkup = InlineKeyboardMarkup([[InlineKeyboardButton("Delete this message", 
-                                                        callback_data="del:%(chat_id)s:%(message_id)s:%(user_id)s" % msdict)]])
-                        msg.edit_reply_markup(reply_markup=kbrmrkup)
+                else:
+                    msg = message.reply_text(reply.text,
+                                             parse_mode=reply.parse_mode,
+                                             reply_markup=reply.inline_keyboard)
+                if reply.failed:
+                    msdict = msg.to_dict()
+                    msdict["chat_id"] = msg.chat_id
+                    msdict["user_id"] = update.message.from_user.id
+                    kbrmrkup = InlineKeyboardMarkup([[InlineKeyboardButton("Delete this message",
+                                                                           callback_data="del:%(chat_id)s:%(message_id)s:%(user_id)s" % msdict)]])
+                    msg.edit_reply_markup(reply_markup=kbrmrkup)
                 try:
                     TRACKER[msg.message_id] = {
-                        "Requested by":update.message.from_user.name,
-                        "Username":update.message.from_user.username,
-                        "Command":update.message.text
+                        "Requested by": update.message.from_user.name,
+                        "Username": update.message.from_user.username,
+                        "Command": update.message.text
                     }
                 except UnboundLocalError:
                     pass
+
 
 def inline_handle(bot: Bot, update: Update):
     query = update.inline_query.query
@@ -149,6 +117,7 @@ def inline_handle(bot: Bot, update: Update):
     result = []
     for command in INLINE:
         if query.startswith(command):
+            print(command)
             reply = INLINE[command](bot, update, user, args)
             TRACK.event(update.inline_query.from_user.id, command, "inline")
             if reply is None:
@@ -180,13 +149,14 @@ def inline_handle(bot: Bot, update: Update):
                         id=uuid4(),
                         title=command,
                         description=reply[0].split("\n")[0],
-                        input_message_content=InputTextMessageContent(reply[0], parse_mode="MARKDOWN")
+                        input_message_content=InputTextMessageContent(
+                            reply[0], parse_mode="MARKDOWN")
                     ))
                 elif reply[1] == constants.PHOTO:
                     if type(reply[0]) == str:
                         result.append(InlineQueryResultPhoto(photo_url=reply[0],
-                                                            thumb_url=reply[0],
-                                                            id=uuid4()))
+                                                             thumb_url=reply[0],
+                                                             id=uuid4()))
                     else:
                         pic = bot.sendPhoto(chat_id=settings.CHANNEL, photo=reply[0])
                         pic = pic.photo[0].file_id
@@ -196,10 +166,10 @@ def inline_handle(bot: Bot, update: Update):
                         ))
                 elif reply[1] == constants.PHOTOWITHINLINEBTN:
                     result.append(InlineQueryResultPhoto(photo_url=reply[0][0],
-                                                        thumb_url=reply[0][0],
-                                                        id=uuid4(),
-                                                        caption=reply[0][1],
-                                                        reply_markup=reply[0][2]))
+                                                         thumb_url=reply[0][0],
+                                                         id=uuid4(),
+                                                         caption=reply[0][1],
+                                                         reply_markup=reply[0][2]))
                 else:
                     raise NotImplementedError("Reply type %s not supported" % reply[1])
             else:
@@ -207,12 +177,16 @@ def inline_handle(bot: Bot, update: Update):
     update.inline_query.answer(results=result,
                                switch_pm_text="List commands",
                                switch_pm_parameter="help")
+
+
 def start_command(_: Bot, update: Update, user, args):
     """/start command"""
     if len(args) != 1:
         return octeon.message("Hi! I am Octeon, a modular telegram bot! To see my features, type /help. \nNews: @octeon")
     else:
         return octeon.message(CMDDOCS)
+
+
 def help_command(bot: Bot, update: Update, user, args):
     """/help command"""
     try:
@@ -222,6 +196,7 @@ def help_command(bot: Bot, update: Update, user, args):
         return octeon.message("PM me, so I can send you /help")
     else:
         return octeon.message("I PMed you /help")
+
 
 def inlinebutton(bot, update):
     query = update.callback_query
@@ -235,7 +210,8 @@ def inlinebutton(bot, update):
             bot.deleteMessage(data[0], data[1])
             query.answer("Message deleted")
         else:
-            query.answer("You are not the one who sent this command!")        
+            query.answer("You are not the one who sent this command!")
+
 
 def onmessage_handle(bot, update):
     for regex in REGEXHAND:
@@ -246,27 +222,30 @@ def onmessage_handle(bot, update):
                 return
             elif reply.photo:
                 msg = message.reply_photo(reply.photo,
-                                            caption=reply.text,
-                                            reply_markup=reply.inline_keyboard)
+                                          caption=reply.text,
+                                          reply_markup=reply.inline_keyboard)
             elif reply.file:
                 msg = message.reply_document(document=reply.file,
-                                                caption=reply.text,
-                                                reply_markup=reply.inline_keyboard)
+                                             caption=reply.text,
+                                             reply_markup=reply.inline_keyboard)
             else:
                 msg = message.reply_text(reply.text,
-                                            parse_mode=reply.parse_mode,
-                                            reply_markup=reply.inline_keyboard)
+                                         parse_mode=reply.parse_mode,
+                                         reply_markup=reply.inline_keyboard)
             if reply.failed:
                 msdict = msg.to_dict()
                 msdict["chat_id"] = msg.chat_id
                 msdict["user_id"] = update.message.from_user.id
-                kbrmrkup = InlineKeyboardMarkup([[InlineKeyboardButton("Delete this message", 
-                                                callback_data="del:%(chat_id)s:%(message_id)s:%(user_id)s" % msdict)]])
+                kbrmrkup = InlineKeyboardMarkup([[InlineKeyboardButton("Delete this message",
+                                                                       callback_data="del:%(chat_id)s:%(message_id)s:%(user_id)s" % msdict)]])
                 msg.edit_reply_markup(reply_markup=kbrmrkup)
+
 
 COMMANDS["/help"] = help_command
 COMMANDS["/start"] = start_command
 COMMANDS["/who_requested"] = tracker
+
+
 def loaded(_: Bot, update: Update):
     """//plugins command"""
     message = "Plugins list:\n"
@@ -277,6 +256,7 @@ def loaded(_: Bot, update: Update):
             message += "✅%s\n" % plugin["name"]
     update.message.reply_text(message)
 
+
 def error_handle(bot, update, error):
     """Handles bad things"""
     if update is None:
@@ -286,6 +266,7 @@ def error_handle(bot, update, error):
     else:
         bot.sendMessage(chat_id=settings.ADMIN,
                         text='Update "{}" caused error "{}"'.format(update, error))
+
 
 LOGGER.info("Checking Analytics status...")
 if settings.TRACKCODE != "":
@@ -306,11 +287,11 @@ if __name__ == '__main__':
     if settings.WEBHOOK_ON:
         LOGGER.info("Webhook is ON")
         UPDATER.start_webhook(listen='0.0.0.0',
-                            port=settings.WEBHOOK_PORT,
-                            url_path=settings.WEBHOOK_URL_PATH,
-                            key=settings.WEBHOOK_KEY,
-                            cert=settings.WEBHOOK_CERT,
-                            webhook_url=settings.WEBHOOK_URL)
+                              port=settings.WEBHOOK_PORT,
+                              url_path=settings.WEBHOOK_URL_PATH,
+                              key=settings.WEBHOOK_KEY,
+                              cert=settings.WEBHOOK_CERT,
+                              webhook_url=settings.WEBHOOK_URL)
     else:
         LOGGER.info("Webhook is OFF")
         UPDATER.start_polling(clean=True)
--- octeon.py
+++ octeon.py
@@ -1,7 +1,11 @@
 """
 Octeon stuff
 """
+import constants
+
 NOTAPLUGIN = True
+
+
 class message:
     """
     Base message class
@@ -9,11 +13,12 @@ class message:
     Raises:
         TypeError on incompatible conditions, like photo and parse_mode
     """
+
     def __init__(self,
-                 text="", 
-                 photo=None, 
+                 text="",
+                 photo=None,
                  file=None,
-                 inline_keyboard=None, 
+                 inline_keyboard=None,
                  parse_mode=None,
                  failed=False):
         self.text = text
@@ -27,31 +32,60 @@ class message:
         if (photo or file) and parse_mode:
             raise TypeError("parse_mode and photo/file cant be used at same time!")
 
+    @classmethod
+    def from_old_format(cls, reply):
+        message = cls()
+        if isinstance(reply, str):
+            message.text = reply
+        elif reply is None:
+            return
+        elif reply[1] == constants.TEXT:
+            message.text = reply[0]
+        elif reply[1] == constants.MDTEXT:
+            message.text = reply[0]
+            message.parse_mode = "MARKDOWN"
+        elif reply[1] == constants.HTMLTXT:
+            message.text = reply[0]
+            message.parse_mode = "HTML"
+        elif reply[1] == constants.NOTHING:
+            pass
+        elif reply[1] == constants.PHOTO:
+            message.photo = reply[0]
+        elif reply[1] == constants.PHOTOWITHINLINEBTN:
+            message.photo = reply[0][0]
+            message.text = reply[0][1],
+            message.inline_keyboard = reply_markup = reply[0][2]
+        if "failed" in reply:
+            message.failed = True
+        return message
+
+
 class Plugin:
     """Octeon plugin base"""
+
     def __init__(self):
         self.commands = []
         self.handlers = []
-    
+
     def command(self, command, description="", inline_supported=True, hidden=False):
         def decorator(func):
             self.commands.append({
-                "command":command,
-                "description":description,
-                "function":func,
-                "inline_support":inline_supported,
-                "hidden":hidden
+                "command": command,
+                "description": description,
+                "function": func,
+                "inline_support": inline_supported,
+                "hidden": hidden
             })
         return decorator
-    
-    def message(self, regex:str):
+
+    def message(self, regex: str):
         """
         Pass regex pattern for your function
         Disclaimer: this may work in Octeon-Discord
         """
         def decorator(func):
             self.handlers.append({
-                "regex":regex,
-                "function":func,
+                "regex": regex,
+                "function": func,
             })
         return decorator
