c97afce89b9a9a1199aa47fc13041a16b094d366
Yan <nezd@protonmail.com>
2017-07-26 22:44:36 +0300

    Introducing Octeon-Pinky, a new Octeon moduleloader
    New features:
    Reload all plugins
    Load specific plugin
    Chat admins would be able to disable specific plugins in their chats
    soon

--- bot.py
+++ bot.py
@@ -30,12 +30,8 @@ TRACKER = {}
 BANNEDUSERS = []
 LOGGER = logging.getLogger("Octeon-Brain")
 UPDATER = Updater(settings.TOKEN)
-PLUGINS = moduleloader.load_plugins(UPDATER)
+PINKY = moduleloader.Pinky()
 DISPATCHER = UPDATER.dispatcher
-COMMANDS = moduleloader.gen_commands(PLUGINS)
-INLINE = moduleloader.gen_inline(PLUGINS)
-REGEXHAND = moduleloader.gen_messages(PLUGINS)
-CMDDOCS = moduleloader.generate_docs(PLUGINS)
 
 
 def tracker(_: Bot, update: Update, __, ___):
@@ -58,160 +54,24 @@ def command_handle(bot: Bot, update: Update):
         update.message.reply_to_message.text = update.message.reply_to_message.caption
     commanddata = update.message.text.split()[0].split('@')
     if (len(commanddata) >= 2 and commanddata[1] == bot.username) or (len(commanddata) == 1):
-        for command in COMMANDS:
-            state_only_command = update.message.text == command or update.message.text.startswith(
-                command + " ")
-            state_word_swap = len(update.message.text.split(
-                "/")) > 2 and update.message.text.startswith(command)
-            state_mention_command = update.message.text.startswith(command + "@")
-            if state_only_command or state_word_swap or state_mention_command:
-                if len(TRACKER) > 100:
-                    TRACKER = {}
-                user = update.message.from_user
-                LOGGER.info("User %s requested %s.", user.username, update.message.text)
-                args = update.message.text.split(" ")[1:]
-                if update.message.reply_to_message is None:
-                    message = update.message
-                else:
-                    message = update.message.reply_to_message
-                reply = COMMANDS[command](bot, update, user, args)
-                TRACK.event(update.message.from_user.id, command, "command")
-                if reply is None:
-                    return
-                elif not isinstance(reply, octeon.message):
-                    # Backwards compability
-                    reply = octeon.message.from_old_format(reply)
-                if reply.photo:
-                    msg = message.reply_photo(reply.photo,
-                                              caption=reply.text,
-                                              reply_markup=reply.inline_keyboard)
-                elif reply.file:
-                    msg = message.reply_document(document=reply.file,
-                                                 caption=reply.text,
-                                                 reply_markup=reply.inline_keyboard)
-                else:
-                    msg = message.reply_text(reply.text,
-                                             parse_mode=reply.parse_mode,
-                                             reply_markup=reply.inline_keyboard)
-                if reply.failed:
-                    msdict = msg.to_dict()
-                    msdict["chat_id"] = msg.chat_id
-                    msdict["user_id"] = update.message.from_user.id
-                    kbrmrkup = InlineKeyboardMarkup([[InlineKeyboardButton("Delete this message",
-                                                                           callback_data="del:%(chat_id)s:%(message_id)s:%(user_id)s" % msdict)]])
-                    msg.edit_reply_markup(reply_markup=kbrmrkup)
-                try:
-                    TRACKER[msg.message_id] = {
-                        "Requested by": update.message.from_user.name,
-                        "Username": update.message.from_user.username,
-                        "Command": update.message.text
-                    }
-                except UnboundLocalError:
-                    pass
-
-
-def inline_handle(bot: Bot, update: Update):
-    query = update.inline_query.query
-    args = query.split(" ")[1:]
-    user = update.inline_query.from_user
-    result = []
-    for command in INLINE:
-        if query.startswith(command):
-            reply = INLINE[command](bot, update, user, args)
-            TRACK.event(update.inline_query.from_user.id, command, "inline")
+        pinkyresp = PINKY.handle_command(update)
+        if pinkyresp:
+            if len(TRACKER) > 100:
+                TRACKER = {}
+            user = update.message.from_user
+            # LOGGER.info("User %s requested %s.", user.username, update.message.text)
+            args = update.message.text.split(" ")[1:]
+            if update.message.reply_to_message is None:
+                message = update.message
+            else:
+                message = update.message.reply_to_message
+            reply = pinkyresp(bot, update, user, args)
             if reply is None:
                 return
-            if not isinstance(reply, octeon.message):
+            elif not isinstance(reply, octeon.message):
+                # Backwards compability
                 reply = octeon.message.from_old_format(reply)
-            if reply.parse_mode == "HTML":
-                result.append(InlineQueryResultArticle(
-                    id=uuid4(),
-                    title=command,
-                    description=re.sub(cleanr, "", reply.text.split("\n")[0]),
-                    input_message_content=InputTextMessageContent(reply.text, parse_mode="HTML")
-                ))
-            elif reply.parse_mode == "MARKDOWN":
-                result.append(InlineQueryResultArticle(
-                    id=uuid4(),
-                    title=command,
-                    description=reply.text.split("\n")[0],
-                    input_message_content=InputTextMessageContent(
-                        reply.text, parse_mode="MARKDOWN")
-                ))
-            elif reply.photo:
-                if type(reply.photo) == str:
-                    result.append(InlineQueryResultPhoto(photo_url=reply.photo,
-                                                         thumb_url=reply.photo,
-                                                         id=uuid4()))
-                else:
-                    pic = bot.sendPhoto(chat_id=settings.CHANNEL, photo=reply.photo)
-                    pic = pic.photo[0].file_id
-                    result.append(InlineQueryResultCachedPhoto(
-                        photo_file_id=pic,
-                        caption=reply.text,
-                        id=uuid4()
-                    ))
-            elif not reply.text == "":
-                result.append(InlineQueryResultArticle(
-                    id=uuid4(),
-                    title=command,
-                    description=reply.text.split("\n")[0],
-                    input_message_content=InputTextMessageContent(reply.text)
-                ))
-            elif reply.photo and reply.inline_keyboard:
-                result.append(InlineQueryResultPhoto(photo_url=reply.photo,
-                                                     thumb_url=reply.photo,
-                                                     id=uuid4(),
-                                                     caption=reply.text,
-                                                     reply_markup=reply.inline_keyboard))
-    update.inline_query.answer(results=result,
-                               switch_pm_text="List commands",
-                               switch_pm_parameter="help",
-                               cache_time=1)
-
-
-def start_command(_: Bot, update: Update, user, args):
-    """/start command"""
-    if len(args) != 1:
-        return octeon.message("Hi! I am Octeon, a modular telegram bot! To see my features, type /help. \nNews: @octeon")
-    else:
-        return octeon.message(CMDDOCS)
-
-
-def help_command(bot: Bot, update: Update, user, args):
-    """/help command"""
-    try:
-        bot.sendMessage(update.message.from_user.id, CMDDOCS)
-        return None
-    except TelegramError:
-        return octeon.message("PM me, so I can send you /help")
-    else:
-        return octeon.message("I PMed you /help")
-
-
-def inlinebutton(bot, update):
-    query = update.callback_query
-    if query.data.startswith("del"):
-        data = query.data.split(":")[1:]
-        goodpeople = [int(data[2]), settings.ADMIN]
-        if data[0].startswith("-"):
-            for admin in bot.getChatAdministrators(data[0]):
-                goodpeople.append(int(admin.user.id))
-        if int(query.from_user.id) in goodpeople:
-            bot.deleteMessage(data[0], data[1])
-            query.answer("Message deleted")
-        else:
-            query.answer("You are not the one who sent this command!")
-
-
-def onmessage_handle(bot, update):
-    for regex in REGEXHAND:
-        if re.match(regex["regex"], update.message.text):
-            reply = regex["function"](bot, update)
-            message = update.message
-            if reply is None:
-                continue
-            elif reply.photo:
+            if reply.photo:
                 msg = message.reply_photo(reply.photo,
                                           caption=reply.text,
                                           reply_markup=reply.inline_keyboard)
@@ -230,22 +90,118 @@ def onmessage_handle(bot, update):
                 kbrmrkup = InlineKeyboardMarkup([[InlineKeyboardButton("Delete this message",
                                                                        callback_data="del:%(chat_id)s:%(message_id)s:%(user_id)s" % msdict)]])
                 msg.edit_reply_markup(reply_markup=kbrmrkup)
+            try:
+                TRACKER[msg.message_id] = {
+                    "Requested by": update.message.from_user.name,
+                    "Username": update.message.from_user.username,
+                    "Command": update.message.text
+                }
+            except UnboundLocalError:
+                pass
+#
+#
 
 
-COMMANDS["/help"] = help_command
-COMMANDS["/start"] = start_command
-COMMANDS["/who_requested"] = tracker
+def inline_handle(bot: Bot, update: Update):
+    query = update.inline_query.query
+    args = query.split(" ")[1:]
+    user = update.inline_query.from_user
+    result = []
+    pinkyresp = PINKY.handle_inline(update)
+    if pinkyresp:
+        reply = pinkyresp[0](bot, update, user, args)
+        if reply is None:
+            return
+        if not isinstance(reply, octeon.message):
+            reply = octeon.message.from_old_format(reply)
+        if reply.parse_mode == "HTML":
+            result.append(InlineQueryResultArticle(
+                id=uuid4(),
+                title=pinkyresp[1],
+                description=re.sub(cleanr, "", reply.text.split("\n")[0]),
+                input_message_content=InputTextMessageContent(reply.text, parse_mode="HTML")
+            ))
+        elif reply.parse_mode == "MARKDOWN":
+            result.append(InlineQueryResultArticle(
+                id=uuid4(),
+                title=pinkyresp[1],
+                description=reply.text.split("\n")[0],
+                input_message_content=InputTextMessageContent(
+                    reply.text, parse_mode="MARKDOWN")
+            ))
+        elif reply.photo:
+            if type(reply.photo) == str:
+                result.append(InlineQueryResultPhoto(photo_url=reply.photo,
+                                                     thumb_url=reply.photo,
+                                                     id=uuid4()))
+            else:
+                pic = bot.sendPhoto(chat_id=settings.CHANNEL, photo=reply.photo)
+                pic = pic.photo[0].file_id
+                result.append(InlineQueryResultCachedPhoto(
+                    photo_file_id=pic,
+                    caption=reply.text,
+                    id=uuid4()
+                ))
+        elif not reply.text == "":
+            result.append(InlineQueryResultArticle(
+                id=uuid4(),
+                title=pinkyresp[1],
+                description=reply.text.split("\n")[0],
+                input_message_content=InputTextMessageContent(reply.text)
+            ))
+        elif reply.photo and reply.inline_keyboard:
+            result.append(InlineQueryResultPhoto(photo_url=reply.photo,
+                                                 thumb_url=reply.photo,
+                                                 id=uuid4(),
+                                                 caption=reply.text,
+                                                 reply_markup=reply.inline_keyboard))
+    update.inline_query.answer(results=result,
+                               switch_pm_text="List commands",
+                               switch_pm_parameter="help",
+                               cache_time=1)
 
 
-def loaded(_: Bot, update: Update):
-    """//plugins command"""
-    message = "Plugins list:\n"
-    for plugin in PLUGINS:
-        if plugin["state"] == constants.ERROR:
-            message += "⛔%s\n" % plugin["name"]
+def inlinebutton(bot, update):
+    query = update.callback_query
+    if query.data.startswith("del"):
+        data = query.data.split(":")[1:]
+        goodpeople = [int(data[2]), settings.ADMIN]
+        if data[0].startswith("-"):
+            for admin in bot.getChatAdministrators(data[0]):
+                goodpeople.append(int(admin.user.id))
+        if int(query.from_user.id) in goodpeople:
+            bot.deleteMessage(data[0], data[1])
+            query.answer("Message deleted")
         else:
-            message += "✅%s\n" % plugin["name"]
-    update.message.reply_text(message)
+            query.answer("You are not the one who sent this command!")
+
+
+def onmessage_handle(bot, update):
+    pinkyresp = PINKY.handle_message(update)
+    for handle in pinkyresp:
+        reply = handle(bot, update)
+        message = update.message
+        if reply is None:
+            continue
+        elif reply.photo:
+            msg = message.reply_photo(reply.photo,
+                                      caption=reply.text,
+                                      reply_markup=reply.inline_keyboard)
+        elif reply.file:
+            msg = message.reply_document(document=reply.file,
+                                         caption=reply.text,
+                                         reply_markup=reply.inline_keyboard)
+        else:
+            msg = message.reply_text(reply.text,
+                                     parse_mode=reply.parse_mode,
+                                     reply_markup=reply.inline_keyboard)
+        if reply.failed:
+            msdict = msg.to_dict()
+            msdict["chat_id"] = msg.chat_id
+            msdict["user_id"] = update.message.from_user.id
+            kbrmrkup = InlineKeyboardMarkup([[InlineKeyboardButton("Delete this message",
+                                                                   callback_data="del:%(chat_id)s:%(message_id)s:%(user_id)s" % msdict)]])
+            msg.edit_reply_markup(reply_markup=kbrmrkup)
 
 
 def error_handle(bot, update, error):
@@ -271,7 +227,6 @@ if __name__ == '__main__':
     LOGGER.info("Adding handlers...")
     DISPATCHER.add_handler(MessageHandler(Filters.text, onmessage_handle))
     DISPATCHER.add_handler(MessageHandler(Filters.command, command_handle))
-    DISPATCHER.add_handler(CommandHandler("/plugins", loaded), group=-1)
     DISPATCHER.add_handler(InlineQueryHandler(inline_handle))
     DISPATCHER.add_handler(CallbackQueryHandler(inlinebutton))
     DISPATCHER.add_error_handler(error_handle)
--- moduleloader.py
+++ moduleloader.py
@@ -1,116 +1,158 @@
 """
-Plugin loader from Octeon rewrite
+Plugin loader from Octeon - Pinky
 """
-from importlib import import_module
-from logging import getLogger
+import importlib.util
+import os.path
 from glob import glob
+from logging import getLogger
+import re
+
 from telegram.ext import Updater
+
+import octeon
+import settings
 from constants import ERROR, OK
 
-LOGGER = getLogger("ModuleLoader")
+LOGGER = getLogger("Octeon-Pinky")
 
 
-def load_plugins(updater: Updater):
-    """
-    Loads plugins.
-    Returns list with dict
-    """
-    LOGGER.info("Loading modules")
-    i = -10000
-    plugins = []
-    for plugin in glob("plugins/*.py"):
-        i += 1
-        LOGGER.debug("Loading plugin %s", plugin)
-        name = plugin.replace("\\", ".").replace("/", ".")[:-3]
-        plugin = "%s" % name
+class Pinky:
+    def __init__(self):
+        self.plugins = []
+        LOGGER.info("Starting Octeon-Pinky. Loading plugins.")
+        self.load_all_plugins()
+
+    def coreplug_reload(self, bot, update, user, *__):
+        if user.id == settings.ADMIN:
+            LOGGER.info("Reload requested.")
+            update.message.reply_text("Reloading modules. ")
+            self.load_all_plugins()
+            return self.coreplug_list()
+        else:
+            return octeon.message("Access Denied.")
+
+    def coreplug_start(self, *_):
+        return octeon.message("Hi! I am Octeon, telegram bot with random stuff!\nTo see my commands, type: /help")
+
+    def coreplug_help(self, *_):
+        docs = ""
+        for plugin in self.plugins:
+            for command in plugin["commands"]:
+                if "description" in command:
+                    if "hidden" in command:
+                        if command["hidden"]:
+                            continue
+                    docs += "%s - %s\n" % (command["command"],
+                                           command["description"])
+        return octeon.message(docs)
+
+    def coreplug_list(self, *_):
+        message = []
+        for plugin in self.plugins:
+            txt = ''
+            if plugin["state"] == OK:
+                txt += "✅"
+            else:
+                txt += "⛔"
+            txt += plugin["name"]
+            message.append(txt)
+        message = sorted(message)
+        message.reverse()
+        return octeon.message("\n".join(message))
+
+    def coreplug_load(self, bot, update, user, args):
+        args = " ".join(args)
+        if user.id == settings.ADMIN:
+            LOGGER.info("Reload requested.")
+            update.message.reply_text("Loading " + args)
+            self.load_plugin(args)
+            return self.coreplug_list()
+        else:
+            return octeon.message("Access Denied.")
+
+    def load_all_plugins(self):
+        self.plugins.clear()
+        for filename in glob("plugins/*.py"):
+            self.load_plugin(filename)
+        self.plugins.append({
+            "state": OK,
+            "name": "Octeon Core Plugin",
+            "commands": [{"command": "/start", "function": self.coreplug_start},
+                         {"command": "//plugins", "function": self.coreplug_list},
+                         {"command": "/help", "function": self.coreplug_help},
+                         {"command": "//reload", "function": self.coreplug_reload},
+                         {"command": "//pluginload", "function": self.coreplug_load}],
+            "messagehandles": [],
+            "disabledin": []
+        })
+
+    def load_plugin(self, plugpath):
+        plugname = os.path.basename(plugpath).split(".py")[0]
         try:
-            module = import_module(plugin)
-            module.level = i
-        except Exception as ohno:  # pylint: disable=W0703
-            LOGGER.error("Module %s failed to load:", plugin)
-            LOGGER.error(ohno)
-            plugins.append({
+            spec = importlib.util.spec_from_file_location("octeon.plugin", plugpath)
+            plugin = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(plugin)
+        except Exception as f:
+            LOGGER.warning("Plugin %s failed to init cause of %s", plugname, f)
+            self.plugins.append({
                 "state": ERROR,
-                "name": name,
-                "commands": []
+                "name": plugname,
+                "commands": [],
+                "disabledin": []
             })
         else:
             try:
-                module.NOTAPLUGIN
+                plugin.PLUGINVERSION
             except AttributeError:
-                try:
-                    module.PLUGINVERSION
-                except AttributeError:
-                    # Working with outdated plugins.
-                    module.preload(updater, i)
-                    plugins.append({
-                        "state": OK,
-                        "name": name,
-                        "commands": module.COMMANDS
-                    })
-                    LOGGER.info("Module %s loaded", name)
-                    LOGGER.warning("Module %s is outdated!", name)
-                else:
-                    # Working with new plugins
-                    plugins.append({
-                        "state": OK,
-                        "name": name,
-                        "commands": module.plugin.commands,
-                        "msghandles": module.plugin.handlers
-                    })
-                    LOGGER.info("Module %s loaded", name)
+                # Working with outdated plugins.
+                self.plugins.append({
+                    "state": OK,
+                    "name": plugname,
+                    "commands": plugin.COMMANDS,
+                    "messagehandles": [],
+                    "disabledin": []
+                })
+                LOGGER.info("Legacy module %s loaded", plugname)
             else:
+                # Working with new plugins
+                self.plugins.append({
+                    "state": OK,
+                    "name": plugname,
+                    "commands": plugin.plugin.commands,
+                    "messagehandles": plugin.plugin.handlers,
+                    "disabledin": []
+                })
+                LOGGER.info("Module %s loaded", plugname)
+
+    def handle_command(self, update):
+        for plugin in self.plugins:
+            if update.message.chat.id in plugin["disabledin"]:
                 continue
-    return plugins
+            else:
+                for command_info in plugin["commands"]:
+                    command = command_info["command"]
+                    function = command_info["function"]
+                    state_only_command = update.message.text == command or update.message.text.startswith(
+                        command + " ")
+                    state_word_swap = len(update.message.text.split(
+                        "/")) > 2 and update.message.text.startswith(command)
+                    state_mention_command = update.message.text.startswith(command + "@")
+                    if state_only_command or state_word_swap or state_mention_command:
+                        return function
 
+    def handle_inline(self, update):
+        for plugin in self.plugins:
+            for command_info in plugin["commands"]:
+                command = command_info["command"]
+                function = command_info["function"]
+                if update.inline_query.query.startswith(command):
+                    return function, command
 
-def gen_commands(pluglist):
-    """
-    Creates list of commands and corresponding function
-    """
-    LOGGER.info("Creating commands...")
-    commands = {}
-    for plugin in pluglist:
-        for command in plugin["commands"]:
-            commands[command["command"]] = command["function"]
-    return commands
-
-
-def gen_inline(pluglist):
-    """
-    Creates list of inline-compatible commands and corresponding function
-    """
-    LOGGER.info("Generating inline commands...")
-    commands = {}
-    for plugin in pluglist:
-        for command in plugin["commands"]:
-            if command["inline_support"]:
-                commands[command["command"]] = command["function"]
-    return commands
-
-
-def gen_messages(pluglist):
-    LOGGER.info("Generating message hadnlers")
-    regexes = []
-    for plugin in pluglist:
-        if "msghandles" in plugin:
-            for handle in plugin["msghandles"]:
-                regexes.append(handle)
-    return regexes
-
-
-def generate_docs(pluglist):
-    """
-    Generates documentation string for
-    commands
-    """
-    docs = ""
-    LOGGER.info("Generating /help command...")
-    for plugin in pluglist:
-        for command in plugin["commands"]:
-            if "hidden" in command:
-                if command["hidden"]:
-                    continue
-            docs += "%s - %s\n" % (command["command"],
-                                   command["description"])
-    return docs
+    def handle_message(self, update):
+        handlers = []
+        for plugin in self.plugins:
+            if "messagehandles" in plugin:
+                for message_info in plugin["messagehandles"]:
+                    if re.match(message_info["regex"], update.message.text):
+                        handlers.append(message_info["function"])
+        return handlers
