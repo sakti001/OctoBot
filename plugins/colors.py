946dc085201e365654e91f850064b14315beebb5
Yan <nezd@protonmail.com>
2017-08-08 13:29:14 +0300

    Upgraded colors.py to new format

--- plugins/colors.py
+++ plugins/colors.py
@@ -4,20 +4,53 @@ from io import BytesIO
 from PIL import Image, ImageColor
 from telegram import Bot, Update
 from telegram.ext import CommandHandler, Updater
+import octeon
+PLUGINVERSION = 2
+# Always name this variable as `plugin`
+# If you dont, module loader will fail to load the plugin!
+plugin = octeon.Plugin()
+@plugin.command(command="/color",
+                description="Create color samples",
+                inline_supported=True,
+                hidden=False)
+def rgb(b: Bot, u: Update, user, args):
+    """
+    Create color samples.
+    Supports both HEX and RGB
+    Example:
+    User:
+    /color #FF0000
 
-import constants
+    Octeon:
+    [ Photo ]
 
-def preload(*_):
-    return
+    Octeon:
+    #FF0000
 
+    User:
+    /color 255 0 0
 
-def rgb(b: Bot, u: Update, user, args):  
+    Octeon:
+    [ Photo ]
+
+    Octeon Dev:
+    [255, 0, 0]
+
+    User:
+    /color 0xFF 0x0 0x0
+
+    Octeon:
+    [ Photo ]
+
+    Octeon:
+    [255, 0, 0]
+    """
     if args[0].startswith("#"):
         color = args[0]
         try:
             usercolor = ImageColor.getrgb(color)
         except Exception:
-            return "Invalid Color Code supplied!", constants.TEXT, "failed"
+            return octeon.message("Invalid Color Code supplied", failed=True)
     elif args[0].startswith("0x"):
         if len(args) > 2:
             usercolor = int(args[0][2:], 16), int(
@@ -27,18 +60,9 @@ def rgb(b: Bot, u: Update, user, args):
             usercolor = ImageColor.getrgb(color)
     else:
         usercolor = int(args[0]), int(args[1]), int(args[2])
+    color = usercolor
     im = Image.new(mode="RGB", size=(128, 128), color=usercolor)
     file = BytesIO()
     im.save(file, "PNG")
     file.seek(0)
-    return file, constants.PHOTO
-
-
-COMMANDS = [
-    {
-        "command":"/rgb",
-        "function":rgb,
-        "description":"Sends specifed color sample. Example inputs: 0x00 0x00 0xFF, #FFFFFF, 255 0 0",
-        "inline_support":True
-    }
-]
+    return octeon.message(text=color, photo=file)
