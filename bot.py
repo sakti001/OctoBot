08d5718485760ad691cd607a1d372da830a88d14
Yan <nezd@protonmail.com>
2017-08-10 15:48:43 +0300

    Removed API-dependent code from core.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ from telegram.ext import (CallbackQueryHandler, CommandHandler, Filters,
                           InlineQueryHandler, MessageHandler, Updater)
 
 import constants
-import moduleloader
+import core
 import octeon
 import settings
 from telegram.ext.dispatcher import run_async
@@ -31,7 +31,100 @@ BANNEDUSERS = []
 LOGGER = logging.getLogger("Octeon-Brain")
 UPDATER = Updater(settings.TOKEN)
 DISPATCHER = UPDATER.dispatcher
-PINKY = moduleloader.Pinky(DISPATCHER)
+
+class Octeon_PTB(core.OcteonCore):
+    def __init__(self, dispatcher):
+        self.dispatcher = dispatcher
+        self.platform = "Telegram"
+        moduleloader.OcteonCore.__init__(self)
+
+    def gen_help(self):
+        docs = ""
+        for plugin in self.plugins:
+            for command in plugin["commands"]:
+                if "description" in command:
+                    if "hidden" in command:
+                        if command["hidden"]:
+                            continue
+                    docs += "%s - <i>%s</i>\n" % (command["command"],
+                                           command["description"])
+        docs += "\nYou can find more info about command by typing after /help, like this: <pre>/help /cash</pre>"
+        return docs
+
+    def create_command_handler(self, command, function):
+        def handler(bot, update, args):
+            if update.message.chat.id in self.disabled:
+                return
+            else:
+                state_only_command = update.message.text == command or update.message.text.startswith(
+                    command + " ")
+                state_word_swap = len(update.message.text.split(
+                    "/")) > 2 and update.message.text.startswith(command)
+                state_mention_command = update.message.text.startswith(command + "@")
+                if state_only_command or state_word_swap or state_mention_command:
+                    reply = function(bot, update, update.message.from_user, args)
+                    message = update.message
+                    if reply is None:
+                        return
+                    elif not isinstance(reply, octeon.message):
+                        # Backwards compability
+                        reply = octeon.message.from_old_format(reply)
+                    if reply.photo:
+                        msg = message.reply_photo(reply.photo)
+                        if reply.text:
+                            msg = message.reply_text(reply.text,
+                                                     parse_mode=reply.parse_mode,
+                                                     reply_markup=reply.inline_keyboard)
+                    elif reply.file:
+                        msg = message.reply_document(document=reply.file,
+                                                     caption=reply.text,
+                                                     reply_markup=reply.inline_keyboard)
+                    else:
+                        msg = message.reply_text(reply.text,
+                                                 parse_mode=reply.parse_mode,
+                                                 reply_markup=reply.inline_keyboard)
+                    if reply.failed:
+                        msdict = msg.to_dict()
+                        msdict["chat_id"] = msg.chat_id
+                        msdict["user_id"] = update.message.from_user.id
+                        kbrmrkup = InlineKeyboardMarkup([[InlineKeyboardButton("Delete this message",
+                                                                               callback_data="del:%(chat_id)s:%(message_id)s:%(user_id)s" % msdict)]])
+                        msg.edit_reply_markup(reply_markup=kbrmrkup)
+        if not command.endswith("/"):
+            self.dispatcher.add_handler(CommandHandler(command=command[1:], callback=handler, pass_args=True))
+
+    def coreplug_start(self, bot, update, user, args):
+        if len(args) > 0:
+            if args[0] == "help" and update.message.chat.type == "private":
+                return octeon.message(self.gen_help(), parse_mode="HTML")
+        kbd = InlineKeyboardMarkup(
+        [
+        [InlineKeyboardButton(text="List commands in PM", url="http://t.me/%s?start=help" % bot.getMe().username)],
+        [InlineKeyboardButton(text="News about Octeon", url=settings.NEWS_LINK)],
+        [InlineKeyboardButton(text="Chat about Octeon", url=settings.CHAT_LINK)],
+        ]
+        )
+        return octeon.message("Hi! I am Octeon, %s bot with random stuff!\nTo see my commands, type: /help" % self.platform, inline_keyboard=kbd)
+
+    def coreplug_help(self, bot, update, user, args):
+        if args:
+            for plugin in self.plugins:
+                for command in plugin["commands"]:
+                    if args[0].lower() == command["command"].lower():
+                        info = {"command":args[0], "description":"Not available", "docs":"Not available"}
+                        info["description"] = command["description"]
+                        if command["function"].__doc__:
+                            info["docs"] = html.escape(textwrap.dedent(command["function"].__doc__))
+                        return octeon.message(COMMAND_INFO % info, parse_mode="HTML")
+            return "I dont know this command"
+        else:
+            if update.message.chat.type == "private":
+                return octeon.message(self.gen_help(), parse_mode="HTML")
+            else:
+                keyboard = InlineKeyboardMarkup([[InlineKeyboardButton(text="List commands in PM", url="http://t.me/%s?start=help" % bot.getMe().username)]])
+                return octeon.message("To prevent flood, use this command in PM", inline_keyboard=keyboard)
+
+PINKY = Octeon_PTB(DISPATCHER)
 
 
 def tracker(_: Bot, update: Update, __, ___):
