84425421fba56b97a3551402cfcb65d1e03785cc
Yan <nezd@protonmail.com>
2017-09-16 13:28:07 +0300

    Improved errors

--- obupdater.py
+++ obupdater.py
@@ -32,22 +32,30 @@ class OBUpdater:
 
     def get_updates(self):
         for update in self.bot.get_updates(offset=self.update_id, timeout=1):
-            self.update_id = update.update_id + 1
-            if update.message:
-                if update.message.caption:
-                    update.message.text = update.message.caption
-                if update.message.reply_to_message:
-                    if update.message.reply_to_message.caption:
-                        update.message.reply_to_message.caption = update.message.text
-                if update.message.text.startswith("/"):
-                    self.command_handle(self.bot, update)
-                else:
-                    self.message_handle(self.bot, update)
-            elif update.inline_query:
-                update.message = telegram.Message(0, update.inline_query.from_user, datetime.datetime.now(), update.inline_query.from_user)
-                self.inline_handle(self.bot, update)
-            elif update.callback_query:
-                self.inline_kbd_handle(self.bot, update)
+            try:
+                self.update_id = update.update_id + 1
+                if update.message:
+                    if update.message.caption:
+                        update.message.text = update.message.caption
+                    if update.message.reply_to_message:
+                        if update.message.reply_to_message.caption:
+                            update.message.reply_to_message.caption = update.message.text
+                    if update.message.text:
+                        if update.message.text.startswith("/"):
+                            self.command_handle(self.bot, update)
+                        else:
+                            self.message_handle(self.bot, update)
+                    else:
+                        self.message_handle(self.bot, update)
+                elif update.inline_query:
+                    update.message = telegram.Message(0, update.inline_query.from_user, datetime.datetime.now(), update.inline_query.from_user)
+                    self.inline_handle(self.bot, update)
+                elif update.callback_query:
+                    self.inline_kbd_handle(self.bot, update)
+            except Exception as e:
+                self.logger.error(e)
+                self.bot.sendMessage(
+                    settings.ADMIN, "Uncatched Exception:\n<code>%s</code>\nUpdate:\n<code>%s</code>" % html.escape(traceback.format_exc()), parse_mode="HTML")
 
     def _poll_worker(self):
         while True:
@@ -62,10 +70,6 @@ class OBUpdater:
                 self.logger.error(e)
                 self.bot.sendMessage(
                     settings.ADMIN, "Uncatched TelegramError:\n<code>%s</code>" % html.escape(traceback.format_exc()), parse_mode="HTML")
-            except Exception as e:
-                self.logger.error(e)
-                self.bot.sendMessage(
-                    settings.ADMIN, "Uncatched Exception:\n<code>%s</code>" % html.escape(traceback.format_exc()), parse_mode="HTML")
 
     def start_poll(self):
         try:
