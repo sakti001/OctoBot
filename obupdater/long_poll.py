c20d055c531564d217bf1afee2b4a92fd5d8cb0c
Octo <nezd@protonmail.com>
2018-02-26 20:00:30 +0300

    Fetch updates for multiple bots in same thread

--- obupdater/long_poll.py
+++ obupdater/long_poll.py
@@ -5,37 +5,42 @@ import logging
 import time
 import html
 
-def create_poll(mirror_name, mirror_token, upd_queue, modloader):
-    bot = telegram.Bot(mirror_token)
-    logger = logging.getLogger("Poller-%s" % mirror_name)
-    bot.modloader = modloader
+
+def create_poll(mirrors, upd_queue, modloader):
+    logger = logging.getLogger("Poller")
+
     def update_fetcher_thread():
         logger.info("Starting update polling")
-        update_id = None
-        bot.sendMessage(settings.ADMIN, "Mirror %s connected" % mirror_name)
+        update_id = {}
+        for mirror_name in mirrors:
+            update_id[mirror_name] = None
         while True:
-            try:
-                updates = bot.get_updates(offset=update_id, timeout=1)
-                for update in updates:
-                    upd_queue.put((bot, update))
-                    update_id = update.update_id + 1
-            except telegram.error.NetworkError:
-                time.sleep(1)
-            except telegram.error.Unauthorized:
-                # The user has removed or blocked the bot.
-                update_id += 1
-            except telegram.error.TelegramError as e:
-                logger.error(e)
+            for mirror_name, mirror_token in mirrors.items():
                 try:
-                    bot.sendMessage(
-                        settings.ADMIN, "Uncatched TelegramError:\n<code>%s</code>" % html.escape(traceback.format_exc()),
-                        parse_mode="HTML")
-                except Exception:
-                    logger.error("Unable to send TelegramError report!")
-            except Exception as e:
-                logger.critical("Uncatched error in updater!")
-                logger.error(e)
-                time.sleep(1)
+                    bot = telegram.Bot(mirror_token)
+                    bot.modloader = modloader
+                    updates = bot.get_updates(offset=update_id[mirror_name], timeout=1)
+                    logger.debug("Getting updates from mirror %s", mirror_name)
+                    for update in updates:
+                        upd_queue.put((bot, update))
+                        update_id[mirror_name] = update.update_id + 1
+                except telegram.error.NetworkError:
+                    time.sleep(1)
+                except telegram.error.Unauthorized:
+                    # The user has removed or blocked the bot.
+                    update_id += 1
+                except telegram.error.TelegramError as e:
+                    logger.error(e)
+                    try:
+                        bot.sendMessage(
+                            settings.ADMIN, "Uncatched TelegramError:\n<code>%s</code>" % html.escape(traceback.format_exc()),
+                            parse_mode="HTML")
+                    except Exception:
+                        logger.error("Unable to send TelegramError report!")
+                except Exception as e:
+                    logger.critical("Uncatched error in updater!")
+                    logger.error(e)
+                    time.sleep(1)
 
     logger.info("Update polling function created successfully")
     return update_fetcher_thread
